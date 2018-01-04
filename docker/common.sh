4a5bb64d40ccad35e1f7c4d00f247546a74e8cf2
David Ke Hong <hongkedavid@gmail.com>
2018-01-04 17:29:21 -0500

    Update and rename command.sh to common.sh

--- docker/common.sh
+++ docker/common.sh
@@ -2,10 +2,10 @@
 sudo docker container list
 
 # Attach to a running container 
-sudo docker attach apollo_dev
+sudo docker attach $container_name
  
 # Detach from a running container
 exit
 
 # Run a container
-sudo docker exec -t -i apollo_dev /bin/bash
+sudo docker exec -t -i $container_name /bin/bash
