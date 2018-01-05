28a6c39b1d819579bad007953886db1d38ff9f71
David Ke Hong <hongkedavid@gmail.com>
2018-01-04 23:58:15 -0500

    Update apollo.sh

--- docker/apollo.sh
+++ docker/apollo.sh
@@ -7,14 +7,18 @@ bash docker/scripts/dev_start.sh
 # Attach to the container
 bash docker/scripts/dev_into.sh
 
-# Detach from the container
-exit (or Ctrl-D)
-
+# Within docker container
 # Start module monitor
 bash scripts/bootstrap.sh
 
 # Download demo rosbag
 bash ./docs/demo_guide/rosbag_helper.sh download 
 
+# Set up ROS environment
+source /apollo/bazel-apollo/external/ros/setup.bash
+
 # Relay demo rosbag 
 rosbag play -l ./docs/demo_guide/demo_2.0.bag
+
+# Detach from the container
+exit (or Ctrl-D)
