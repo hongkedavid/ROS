9d11a7d2a969c48a8c245bc7f38d8f6f843ccbf8
David Ke Hong <hongkedavid@gmail.com>
2018-01-13 19:17:21 -0500

    Update common.sh

--- docker/common.sh
+++ docker/common.sh
@@ -1,11 +1,17 @@
+# Add user into docker group if needed, need to re-login to be effective
+# Ref: https://askubuntu.com/questions/136788/how-do-i-list-the-members-of-a-group
+# Ref: https://docs.docker.com/engine/installation/linux/linux-postinstall/#manage-docker-as-a-non-root-user
+getent group docker
+sudo usermod -aG docker $USER
+
 # List running containers 
-sudo docker container list
+docker container list
 
 # Attach to a running container 
-sudo docker attach $container_name
+docker attach $container_name
  
 # Detach from a running container
 exit
 
 # Run a container
-sudo docker exec -t -i $container_name /bin/bash
+docker exec -t -i $container_name /bin/bash
