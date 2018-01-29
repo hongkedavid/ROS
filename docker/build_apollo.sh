77a958aa2d6a4d0bd51ba96cb9ffa1ffa4dd1ded
David Ke Hong <hongkedavid@gmail.com>
2018-01-29 14:40:15 -0500

    Update build_apollo.sh

--- docker/build_apollo.sh
+++ docker/build_apollo.sh
@@ -5,4 +5,12 @@ bazel build modules/canbus:all
 bazel build modules/planning:all
 bazel build modules/prediction:all
 bazel build modules/localization:all
+bazel build modules/routing:all
+bazel build modules/third_party_perception:all
+bazel build modules/drivers/conti_radar:all
+bazel build modules/calibration/lidar_ex_checker:all
+bazel build modules/calibration/republish_msg:all
+
+# Build fail in dreamview and perception
+bazel build modules/perception:all
 bazel build modules/dreamview:all
