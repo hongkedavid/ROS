54f437b464b79634e2efae8b0f8c8f963f0459ba
David Ke Hong <hongkedavid@gmail.com>
2018-01-30 17:27:01 -0500

    Update apollo_llvm.sh

--- docker/apollo_llvm.sh
+++ docker/apollo_llvm.sh
@@ -3,3 +3,18 @@ clang -C -E $file.cc > $file.ii
 
 # llvm bitcode
 clang -O3 -emit-llvm $file.cc -c -o $file.bc
+
+# native assembly
+clang $file.cc -S -o $file.s
+
+# executable binary
+clang $file.cc -o $file
+
+# view bitcode
+llvm-dis < $file.bc | less
+
+# bitcode to assembly
+llc $file.bc -o $file.s
+
+# assembly to executable
+gcc $file.s -o $file
