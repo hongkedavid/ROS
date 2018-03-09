59154dd11cfc9139f7df0618b728c7d8efa41602
David Ke Hong <hongkedavid@gmail.com>
2018-03-09 02:10:01 -0500

    Update stop_apollo.sh

--- docker/stop_apollo.sh
+++ docker/stop_apollo.sh
@@ -7,5 +7,8 @@ supervisorctl stop control
 supervisorctl stop dreamview
 supervisorctl stop monitor
 
+# Stop sim_control
+bash /apollo/scripts/dreamview_sim_control.sh stop
+
 # Optional: stop recording
 scripts/record_bag.sh stop
