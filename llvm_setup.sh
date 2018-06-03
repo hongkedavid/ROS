6c3700ad34da9ec0479a9f66485dad583d96c36f
David Ke Hong <hongkedavid@gmail.com>
2018-06-02 22:21:56 -0400

    Update llvm_setup.sh

--- llvm_setup.sh
+++ llvm_setup.sh
@@ -2,3 +2,6 @@
 sudo apt-get install llvm clang
 cd /usr/share/llvm-3.4/cmake
 sudo ln -s LLVM-Config.cmake LLVMConfig.cmake
+
+# Install LLVM 4.0 on Ubuntu 14
+# Ref: https://releases.llvm.org/4.0.1/docs/CMake.html
