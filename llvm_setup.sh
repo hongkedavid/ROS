636cbddaf7b8dc1ab7f0559dd6bb1bde9206fe6c
David Ke Hong <hongkedavid@gmail.com>
2018-06-07 22:26:08 -0400

    Update llvm_setup.sh

--- llvm_setup.sh
+++ llvm_setup.sh
@@ -4,7 +4,6 @@ cd /usr/share/llvm-3.4/cmake
 sudo ln -s LLVM-Config.cmake LLVMConfig.cmake
 
 # Install LLVM 4.0 (may need to upgrade CMake) on Ubuntu 14
-# Ref: https://releases.llvm.org/4.0.1/docs/CMake.html
 # Ref: https://askubuntu.com/questions/829310/how-to-upgrade-cmake-in-ubuntu
 sudo apt remove cmake
 wget https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.sh
@@ -12,3 +11,9 @@ sudo mv cmake-3.11.3-Linux-x86_64.sh /opt/
 cd /opt
 sudo ./cmake-3.11.3-Linux-x86_64.sh 
 sudo ln -s /opt/cmake-3.11.3-Linux-x86_64/bin/* /usr/bin/
+# Ref: https://releases.llvm.org/4.0.1/docs/CMake.html
+cd llvm-4.0.0.src
+mkdir build
+cd build
+cmake llvm-4.0.0.src/
+. -- -j32
