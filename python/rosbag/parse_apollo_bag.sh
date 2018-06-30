2560a5aeefbdc7e9a6277040748d644791301a2d
David Ke Hong <hongkedavid@gmail.com>
2018-06-30 17:43:21 -0400

    Update parse_apollo_bag.sh

--- python/rosbag/parse_apollo_bag.sh
+++ python/rosbag/parse_apollo_bag.sh
@@ -8,11 +8,8 @@ python dumpbag.py demo.bag demobag_out/ --raw_bag=True
 /usr/local/bin/protoc --encode=apollo.drivers.gnss.Gnss gnss.proto < gnss_msb.pb.txt > gnss_msg.bin
 
 # Generate python API for protocol buffer
+# Ref: https://developers.google.com/protocol-buffers/docs/reference/python-generated
 /usr/local/bin/protoc --proto_path=src --python_out=build src/header.proto
 
 # Dump ros bag files with binary output
 python writebag.py demo.bag demo.np.bag --raw_bag=True
-
-# Generate python API for protobuf
-# Ref: https://developers.google.com/protocol-buffers/docs/reference/python-generated
-protoc --proto_path=/apollo/modules/common/proto --python_out=build/gen src/foo.proto src/bar/baz.proto
