422df61ab9c28aa0581b903b4d85c734e71052c8
David Ke Hong <hongkedavid@gmail.com>
2018-09-24 18:38:56 -0400

    Update common.sh

--- docker/common.sh
+++ docker/common.sh
@@ -1,3 +1,4 @@
+# Install docker-ce at https://docs.docker.com/install/linux/docker-ce/ubuntu/
 # Add user into docker group if needed, need to re-login to be effective
 # Ref: https://askubuntu.com/questions/136788/how-do-i-list-the-members-of-a-group
 # Ref: https://docs.docker.com/engine/installation/linux/linux-postinstall/#manage-docker-as-a-non-root-user
