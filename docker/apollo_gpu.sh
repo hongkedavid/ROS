50388158d37a6def9f7002fba796ed0de58fc27a
David Ke Hong <hongkedavid@gmail.com>
2018-09-24 20:29:55 -0400

    Create apollo_gpu.sh

---
+++ docker/apollo_gpu.sh
@@ -0,0 +1,5 @@
+# NVidia command lines
+nvidia-smi
+sudo lsof | grep nvidia
+
+# Ref: https://github.com/ApolloAuto/apollo/blob/fb12723bd6dcba88ecccb6123ea850da1e050171/docs/howto/how_to_run_perception_module_on_your_local_computer.md
