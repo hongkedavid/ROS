51c9f12df0def18d49427c37f9563ce954bef39a
David Ke Hong <hongkedavid@gmail.com>
2018-10-08 18:37:57 -0400

    Update ucklee_setup.sh

--- ucklee_setup.sh
+++ ucklee_setup.sh
@@ -3,8 +3,8 @@
 # LLVM-2.7 needs to be built by gcc 4.4
 # Ref: https://lists.gt.net/clamav/devel/59892
 sudo apt-get install gcc-4.4 g++-4.4
-sudo ln -s -b /usr/bin/gcc /usr/bin/gcc
-sudo ln -s -b /usr/bin/g++ /usr/bin/g++
+sudo ln -s -b /usr/bin/gcc-4.4 /usr/bin/gcc
+sudo ln -s -b /usr/bin/g++-4.4 /usr/bin/g++
 
 # Ref: http://lists.llvm.org/pipermail/llvm-dev/2008-April/014059.html
 # Ref: http://adamcavendish.is-programmer.com/posts/71212.html
