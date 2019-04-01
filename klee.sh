b65d5635a23fd8be2341a4869c46b307ceea891b
David Ke Hong <hongkedavid@gmail.com>
2019-04-01 15:15:00 -0400

    Update klee.sh

--- klee.sh
+++ klee.sh
@@ -4,7 +4,7 @@ make
 sudo make install
 
 # Run KLEE on Apollo
-cd bazel-bin/modules/planning
+cd /apollo/bazel-bin/modules/planning
 klee --libc=uclibc --posix-runtime --link-llvm-lib=/apollo/boost_1_69_0/o_files/boost.bc --link-llvm-lib=liblog4cxx.so.10.0.0.bc --link-llvm-lib=libroconsole_all.bc --link-llvm-lib=dummy.bc planning.bc
 
 # Generate Apollo bitcode
