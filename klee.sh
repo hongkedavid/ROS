22af4eae4da7c24a2acc74d727fe93d359a844bd
David Ke Hong <hongkedavid@gmail.com>
2019-04-01 15:14:19 -0400

    Update klee.sh

--- klee.sh
+++ klee.sh
@@ -8,5 +8,7 @@ cd bazel-bin/modules/planning
 klee --libc=uclibc --posix-runtime --link-llvm-lib=/apollo/boost_1_69_0/o_files/boost.bc --link-llvm-lib=liblog4cxx.so.10.0.0.bc --link-llvm-lib=libroconsole_all.bc --link-llvm-lib=dummy.bc planning.bc
 
 # Generate Apollo bitcode
-cd bazel-bin/modules/planning
-klee --libc=uclibc --posix-runtime --link-llvm-lib=/apollo/boost_1_69_0/o_files/boost.bc --link-llvm-lib=liblog4cxx.so.10.0.0.bc --link-llvm-lib=libroconsole_all.bc --link-llvm-lib=dummy.bc planning.bc
+cd /apollo
+bazel build --define ARCH=x86_64 --define CAN_CARD=fake_can --cxxopt=-DUSE_ESD_CAN=false --copt=-mavx2 --copt=-mno-sse3 --cxxopt=-DCPU_ONLY --crosstool_top=tools/wllvm:toolchain //modules/planning:planning --compilation_mode=dbg
+cd /apollo/bazel-bin/modules/planning
+extract-bc planning
